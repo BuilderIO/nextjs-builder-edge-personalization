8218d9a53a8d348e70d59ed14cd17be6dd528225
Aziz Abbas <aziz@builder.io>
2021-10-20 14:40:54 -0700

    add comment

--- pages/api/attributes.ts
+++ pages/api/attributes.ts
@@ -5,6 +5,9 @@ if (!process.env.BUILDER_PRIVATE_KEY) {
   throw new Error('No BUILDER_PRIVATE_KEY defined')
 }
 
+/**
+ * API to get the custom targeting attributes from Builder, only needed for the context menu to show a configurator and allow toggling of attributes
+ */
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const attributes = await getAttributes(process.env.BUILDER_PRIVATE_KEY!)
   res.send(attributes)
