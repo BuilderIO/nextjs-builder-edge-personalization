cd6f85e8e8d4d68e311536c9c4a413f60b352468
Aziz Abbas <aziz@builder.io>
2021-10-25 16:14:48 -0700

    remove unused import

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,4 +1,4 @@
-import { NextFetchEvent, NextResponse, NextRequest } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import { getPersonalizedRewrite } from '@builder.io/personalization-utils'
 
 const excludededPrefixes = ['/favicon', '/api']
